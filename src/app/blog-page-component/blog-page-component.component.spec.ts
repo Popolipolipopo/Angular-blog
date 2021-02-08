33a871b9cde8f9ba4df3c32155f718927ca10546
baptistetougouri <baptiste.tougouri@epitech.eu>
2021-02-08 16:55:37 +0100

    Adding missing file for header and nav

---
+++ src/app/blog-page-component/blog-page-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlogPageComponentComponent } from './blog-page-component.component';
+
+describe('BlogPageComponentComponent', () => {
+  let component: BlogPageComponentComponent;
+  let fixture: ComponentFixture<BlogPageComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BlogPageComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BlogPageComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
