77d4a8218fa2dd74d2092663b3d3dd455a967925
baptistetougouri <baptiste.tougouri@epitech.eu>
2021-02-08 16:49:22 +0100

    Starting navigation bar and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,66 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CarousselComponent } from './caroussel/caroussel.component';
+import { LandingPageComponentComponent } from './landing-page-component/landing-page-component.component';
+import { BlogPageComponentComponent } from './blog-page-component/blog-page-component.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './navigation/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+const appRoutes: Routes = [
+  { path: 'home', component: LandingPageComponentComponent },
+  { path: 'blog', component: BlogPageComponentComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarousselComponent
+    CarousselComponent,
+    LandingPageComponentComponent,
+    BlogPageComponentComponent,
+    PageNotFoundComponent,
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
-    MatSliderModule,
+    MatTabsModule,
     MatCardModule,
     BrowserModule,
     MatGridListModule,
+    CommonModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forRoot(
+      appRoutes,
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+  ]
 })
+
 export class AppModule { }
