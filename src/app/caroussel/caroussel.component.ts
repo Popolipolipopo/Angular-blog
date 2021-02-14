d99eb68187d4182f02fe7560c23891b271e9c345
Clyde GUILLOT <clyde.guillot@epitech.eu>
2021-02-14 15:08:16 +0100

    Link landing page caroussel to firestore

--- src/app/caroussel/caroussel.component.ts
+++ src/app/caroussel/caroussel.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {Observable} from 'rxjs';
 
 export interface Tile {
-  color: string;
   text: string;
   image: string;
   content: string;
@@ -15,11 +17,10 @@ export interface Tile {
 export class CarousselComponent implements OnInit {
 
   breakpoint: string;
-  tile: Tile[] = [
-    {text: 'Dodge Charger', content: '', image: 'https://media.gqmagazine.fr/photos/5e528eaa94cd350008180630/master/w_1200,c_limit/fast-furious-1970-dodge-charger.jpg', color: 'white'},
-    {text: 'Mustang Fastback', content: '', image: 'https://www.telegraph.co.uk/content/dam/films/2017/04/05/1967_Mustang-Tokyo_Drift_trans_NvBQzQNjv4BqJKw5vLEHYuL1UeCKr-xp3IGUylfIO2AnqMD-bPhjICI.png?imwidth=450', color: 'white'},
-    {text: 'Chevrolet Chevelle', content: '', image: 'https://www.telegraph.co.uk/content/dam/films/2017/04/05/1967_Mustang-Tokyo_Drift_trans_NvBQzQNjv4BqJKw5vLEHYuL1UeCKr-xp3IGUylfIO2AnqMD-bPhjICI.png?imwidth=450', color: 'white'},
-  ];
+  items: Observable<any[]>;
+  constructor(private store: AngularFirestore, private storage: AngularFireStorage) {
+    this.items = this.store.collection('test').valueChanges();
+  }
   // tslint:disable-next-line:typedef
   ngOnInit() {
     this.breakpoint = (window.innerWidth <= 880) ? '0 1 calc(100% - 16px)' : '0 1 calc(33% - 16px)';
